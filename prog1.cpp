5238bc5659a6ba957ec0af417be11568fe3f1391
Marcela Rosales <marcelarosalesj@gmail.com>
2017-03-14 23:45:30 -0600

    Adding histogram. This would be better as a function instead of inside the main.
    Signed-off-by: Marcela Rosales <marcelarosalesj@gmail.com>

--- prog1.cpp
+++ prog1.cpp
@@ -250,7 +250,7 @@ VideoCapture cap(1);
 
 int main()
 {
-	Mat img;
+	Mat img, binimg;
     short num_seed;
     cout<<"How many regions to find?"<<endl;
         cin >> num_seed;
@@ -259,6 +259,52 @@ int main()
 
 
         cap >> img;
+
+
+        // OpenCV histogram function
+        // http://docs.opencv.org/2.4/doc/tutorials/imgproc/histograms/histogram_calculation/histogram_calculation.html
+        
+        vector<Mat> bgr_planes;
+        split( img, bgr_planes );
+
+
+        int histSize = 256;
+        
+        float range[] = { 0, 256 } ; //the upper boundary is exclusive
+        const float* histRange = { range };
+        
+        bool uniform = true; bool accumulate = false;
+      
+        Mat b_hist, g_hist, r_hist;
+
+        calcHist( &bgr_planes[0], 1, 0, Mat(), b_hist, 1, &histSize, &histRange, uniform, accumulate );
+        calcHist( &bgr_planes[1], 1, 0, Mat(), g_hist, 1, &histSize, &histRange, uniform, accumulate );
+        calcHist( &bgr_planes[2], 1, 0, Mat(), r_hist, 1, &histSize, &histRange, uniform, accumulate );
+
+        // Draw the histograms for B G R
+        int hist_w = 512; int hist_h = 400;
+        int bin_w = cvRound( (double) hist_w/histSize );
+
+        Mat histImage( hist_h, hist_w, CV_8UC3, Scalar(0,0,0) );
+        
+        // Normalize the result to [0, histImage.rows]
+        normalize(b_hist, b_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
+        normalize(g_hist, g_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
+        normalize(r_hist, r_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
+
+        /// Draw for each channel
+        for( int i = 1; i < histSize; i++ ) {
+            line( histImage, Point( bin_w*(i-1), hist_h - cvRound(b_hist.at<float>(i-1)) ) ,
+                       Point( bin_w*(i), hist_h - cvRound(b_hist.at<float>(i)) ),
+                       Scalar( 255, 0, 0), 2, 8, 0  );
+            line( histImage, Point( bin_w*(i-1), hist_h - cvRound(g_hist.at<float>(i-1)) ) ,
+                        Point( bin_w*(i), hist_h - cvRound(g_hist.at<float>(i)) ),
+                        Scalar( 0, 255, 0), 2, 8, 0  );
+            line( histImage, Point( bin_w*(i-1), hist_h - cvRound(r_hist.at<float>(i-1)) ) ,
+                        Point( bin_w*(i), hist_h - cvRound(r_hist.at<float>(i)) ),
+                        Scalar( 0, 0, 255), 2, 8, 0  );
+        }
+        imshow("OpenCV Histogram", histImage );
 
 
         imshow("Cam", img);
@@ -271,11 +317,10 @@ int main()
         }
 
 
+        cvtColor( img, binimg, CV_BGR2GRAY );
+        threshold(binimg, binimg, 50, 255, THRESH_BINARY);
+        imshow("Binary image ", binimg);
 
-        threshold(img, img, 50,255,THRESH_BINARY);
-        imshow("Binary image ", img); // Binarized the image, it only contain 0s and 255s
-        cout << "CH : "<<img.channels()<<endl;
- 
         char key = waitKey(5);
         switch (key) {
             case 'm': cout << "Region Growing Algorithm about to start \n" << endl; regionGrowing(img, num_seed); break;
