e866a5a128f3d50277451f247dd953ba40816ef2
Marcela Rosales <marcelarosalesj@gmail.com>
2017-03-20 21:43:41 -0600

    Adding features: - Captures HSV values given by the user (through clicking on the HSV image) into a colortraining.txt file. For now it ask for 5 input values. - Applies a color filter using the average and standard deviation of those HSV values. - It is also possible to remove the colotTraining.txt file. The user case would be that the HSV input in that file was not good enough and the color filter couldn't distinguish the object of that color. Pending: - Try other filters (blur, median, gaussians with different sigmas or sizes) to increase the robustness of the color filter. In color-noisy environments it's not working, so maybe filtering that would help. Or maybe I'll just try with another background. - Try with other color space (?)
    Signed-off-by: Marcela Rosales <marcelarosalesj@gmail.com>

--- prog1.cpp
+++ prog1.cpp
@@ -3,7 +3,6 @@
 #include <opencv2/highgui/highgui.hpp>
 #include "opencv2/imgproc/imgproc.hpp" // I added this to make it work with opencv3
 //#include <cv.h>
-#include <string>
 #include <iostream>
 #include <fstream>
 #include <queue>
@@ -17,6 +16,12 @@ string file_name;
 bool stop = false;
 bool freeze = false;
 Mat imgClick;
+Mat imgHSV;
+int trainHSVcounter;
+int clickamount;
+
+ofstream outputfile;
+ifstream inputfile;
 
 /*
     Prints 
@@ -24,12 +29,10 @@ Mat imgClick;
     @params binarized image and  number of regions to look for.
     @return nothing.
 */
-void mouseCoordinatesExampleCallback(int event, int x, int y, int flags, void* param)
-{
+void mouseCoordinatesCallback(int event, int x, int y, int flags, void* param) {
     uchar* destination;
     vector<Point> points;
-    int Px;
-    int Py;
+
     int vR;
     int vG;
     int vB;
@@ -50,17 +53,15 @@ void mouseCoordinatesExampleCallback(int event, int x, int y, int flags, void* p
     switch (event)
     {
         case CV_EVENT_LBUTTONDOWN:
-
-            
-
+            cout << "Left button down: Freeze"<<endl;
             freeze = true;
-            Px=x;
-            Py=y;
-            destination = (uchar*) imgClick.ptr<uchar>(Py);
+
+            destination = (uchar*) imgClick.ptr<uchar>(y);
             
-            vB=destination[Px * 3];
-            vG=destination[Px*3+1];
-            vR=destination[Px*3+2];
+            vB=destination[x*3];
+            vG=destination[x*3+1];
+            vR=destination[x*3+2];
+
             //Valores HSV
             rpi = vR / 255.0;
             gpi = vG / 255.0;
@@ -90,24 +91,58 @@ void mouseCoordinatesExampleCallback(int event, int x, int y, int flags, void* p
             }
             vV = Cmax;
             
-            cout << vH << "  " << vS << "  " << vV<< endl;
-            if(conta < 30){
-                Ahsv[conta][0] = vH;
-                Ahsv[conta][1] = vS;
-                Ahsv[conta][2] = vV;
-                conta++;
-            }
-            /*  Draw a point */
-            points.push_back(Point(Px, Py));
+            cout << "RGB (" << vR << "," << vG << "," << vB << ")" << endl;
+            cout << "HSV (" << vH << "," << vS << "," << vV << ")" << endl;       
+            
             break;
         case CV_EVENT_MOUSEMOVE:
             break;
         case CV_EVENT_LBUTTONUP:
             break;
         case CV_EVENT_RBUTTONDOWN:
-            freeze=true;
-            cout << "boton derecho"<<endl;
-        //flag=!flag;
+            cout << "Right button down: Unfreeze"<<endl;
+            freeze=false;
+            break;
+        
+    }
+}
+
+void mouseHSVtrainCallback(int event, int x, int y, int flags, void* param) {
+    uchar* destination;
+    vector<Point> points;
+
+
+
+    switch (event)
+    {
+        case CV_EVENT_LBUTTONDOWN:            
+            destination = (uchar*) imgHSV.ptr<uchar>(y);
+            int ch1, ch2, ch3;
+            ch1 = destination[x*3];
+            ch2 = destination[x*3+1];
+            ch3 = destination[x*3+2];
+            cout << "HSV " << ch1 << "," << ch2 << "," << ch3 << endl;
+
+            if(trainHSVcounter < clickamount){
+                outputfile.open ("colorTraining.txt", std::ios_base::app | std::ios_base::out);
+                outputfile << ch1 << " " << ch2 << " " << ch3 <<  "\n";
+                outputfile.close();
+                trainHSVcounter++;
+            } 
+
+            if(trainHSVcounter == clickamount){
+                cout << " Done with HSV Training. Stop storing" << endl;
+                cvDestroyWindow("HSV Cam");
+                cvDestroyWindow("Gaussian filter");
+            }         
+
+            
+            break;
+        case CV_EVENT_MOUSEMOVE:
+            break;
+        case CV_EVENT_LBUTTONUP:
+            break;
+        case CV_EVENT_RBUTTONDOWN:
             break;
         
     }
@@ -248,7 +283,7 @@ void regionGrowing(Mat img, short num) {
 
 /*
     OpenCV histogram function
-    http://docs.opencv.org/2.4/doc/tutorials/imgproc/histograms/histogram_calculation/histogram_calculation.html    
+    Retrieved from http://docs.opencv.org/2.4/doc/tutorials/imgproc/histograms/histogram_calculation/histogram_calculation.html    
 */
 void get_histogram(Mat img){
     vector<Mat> bgr_planes;
@@ -295,15 +330,126 @@ void get_histogram(Mat img){
 }
 
 
+void showMenu(){
+    cout << "Mode: " << endl;
+    cout << "d) Train the model: HSV Colors" << endl;
+    cout << "f) Train the model: Hu Moments" << endl;
+    cout << "g) Detect an object" << endl;
+    cout << "h) Graph Hu Moments" << endl;
+    cout << "c) Color Filter" << endl;
+    cout << "v) Remove the model" << endl;
+}
+
+
+void trainHSVColors(Mat img){
+
+
+    // Gaussian Kernel 5x5, Sigma=0.5
+    int kernel_size;
+    kernel_size=5;
+    Mat my_kernel;
+    my_kernel= (Mat_<double>(kernel_size,kernel_size) <<
+                0.000002,0.000212,0.000922,0.000212,0.000002,0.000212,0.024745,0.107391,0.024745,0.000212,0.000922,0.107391,0.466066,0.107391,0.000922,0.000212,0.024745,0.107391,0.024745,0.000212,0.000002,0.000212,0.000922,0.000212,0.000002
+    );
+    filter2D(img, img, -1 , my_kernel, Point( -1, -1 ), 0, BORDER_DEFAULT );
+    imshow("Gaussian filter", img);
+
+    // BGR to HSV
+    cvtColor(img, imgHSV, COLOR_BGR2HSV);
+    imshow("HSV Cam", imgHSV);
+    setMouseCallback("HSV Cam", mouseHSVtrainCallback);
+
+    trainHSVcounter = 0;
+    cout << "How many clicks are you going to give? ";
+    //cin >> clickamount;
+    clickamount = 5;
+}
+
+/*
+    Calculate Standard Deviaton
+    Partially retrieved from https://www.programiz.com/cpp-programming/examples/standard-deviation
+*/
+
+double calculateSD(queue<int> data)
+{
+    queue<int> cpydata;
+    cpydata = data;
+    double sum, mean, standardDeviation;
+    sum = 0.0;
+    standardDeviation = 0.0;
+
+    while(!data.empty()){
+        sum += data.front();
+        data.pop();
+    }
+
+    mean = sum/10;
+
+    while(!cpydata.empty()){
+        standardDeviation += pow(cpydata.front() - mean, 2);
+        cpydata.pop();
+    }
+
+    return sqrt(standardDeviation / 10);
+}
+
+
+void colorFilter(Mat img){
+    queue<int> qch1, qch2, qch3;
+    double stddevch1, stddevch2, stddevch3;
+    inputfile.open ("colorTraining.txt", std::ios_base::app | std::ios_base::out);
+    int ch1, ch2, ch3;
+    double avgch1, avgch2, avgch3;
+
+    avgch1 = 0.0;
+    avgch2 = 0.0;
+    avgch3 = 0.0;
+    int cant;
+    cant = 0;
+    while (inputfile >> ch1 >> ch2 >> ch3) {
+        avgch1 += ch1;
+        avgch2 += ch2;
+        avgch3 += ch3;
+        qch1.push(ch1);
+        qch2.push(ch2);
+        qch3.push(ch3);
+        cant++;
+    }
+    avgch1 /= cant;
+    avgch2 /= cant;
+    avgch3 /= cant;
+    stddevch1 = calculateSD(qch1);
+    stddevch2 = calculateSD(qch2);
+    stddevch3 = calculateSD(qch3);
+    cout << " avg= "<< avgch1 << " , " << avgch2 << " , " << avgch3 << endl;
+    cout << " std dev= "<< stddevch1 << " , " << stddevch2 << " , " << stddevch3 << endl;
+    inputfile.close();
+
+    // Start filtering
+    // BGR to HSV
+    cvtColor(img, img, COLOR_BGR2HSV);
+    inRange(img, cv::Scalar(floor(avgch1-stddevch1), floor(avgch2-stddevch2), floor(avgch3-stddevch3)), cv::Scalar(floor(avgch1+stddevch1), floor(avgch2+stddevch2), floor(avgch3+stddevch3) ), img);
+    imshow("HSV Filter", img);
+
+}
+
 VideoCapture cap(0);
 
 int main()
 {
-	Mat img, binimg;
+	Mat img, imgBIN;
     short num_seed;
-    cout<<"How many regions to find?"<<endl;
-    cin >> num_seed;
-
+    
+    // Opening training files. 
+    // Color training will store HSV colors
+    // Object training will store Phis related to object detection
+    // objectTraining;
+    
+    //objectTraining.open ("objectTraining.txt");
+    
+    
+    showMenu();
+    
     while(stop == false){
 
         cap >> img;
@@ -311,28 +457,54 @@ int main()
         get_histogram(img);
 
         imshow("Cam", img);
-        setMouseCallback("Cam", mouseCoordinatesExampleCallback);
+        setMouseCallback("Cam", mouseCoordinatesCallback);
 
         if(!freeze){
             imgClick = img;
             namedWindow("Click");
-            setMouseCallback("Click", mouseCoordinatesExampleCallback);
+            setMouseCallback("Click", mouseCoordinatesCallback);
             imshow("Click", imgClick);
         }
 
-
-        cvtColor( img, binimg, CV_BGR2GRAY );
-        threshold(binimg, binimg, 50, 255, THRESH_BINARY);
-        imshow("Binary image ", binimg);
+        // BGR to GRAY
+        //cvtColor( img, imgBIN, CV_BGR2GRAY );
+        //threshold(imgBIN, imgBIN, 50, 255, THRESH_BINARY);
+        //imshow("Binarized Cam ", imgBIN);
 
         char key = waitKey(5);
         switch (key) {
-            case 'm': cout << "Region Growing Algorithm about to start \n" << endl; regionGrowing(img, num_seed); break;
-            case 27: stop = true; break;
+            case 'd': // Train the model: HSV Colors
+                trainHSVColors(img);
+                break;
+            case 'f': // Train the model: Hu Moments
+
+                break;
+            case 'g': // Detect and object
+                cout<<"How many regions to find?"<<endl;
+                cin >> num_seed;    
+                cout << "Region Growing Algorithm about to start \n" << endl; 
+                regionGrowing(img, num_seed);     
+                break;
+            case 'h': // Graph Hu Moments
+
+
+                break;
+            case 'c': // Color Filter
+                colorFilter(img);
+                break;
+            case 'v':
+                
+                if( remove( "colorTraining.txt" ) != 0 )
+                    cout << "Error deleting file"<<endl;
+                break;
+            case 27: // Exit Program
+                stop = true; 
+                break;
         }
 
+
     }
-    
+
     cout<<"Goodbye!"<<endl;
     return 0;
 }
